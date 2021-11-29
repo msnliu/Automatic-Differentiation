48b55174bb895f98b2441b024e79dfe8e60d23fa
msnliu <zongjunliu@hsph.harvard.edu>
2021-11-29 01:38:02 -0500

    added TODO

---
+++ ad_AHJZ/TODO.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 29 01:15:57 2021
+
+@author: aditimemani
+"""
+
+Code
+
+	⁃	implement comparison operators in the val_derv class (less than, greater than, less than or equal to, greater than or equal to, equal to, not equal to)
+	⁃	implement logistic function 
+	⁃	change the error message in the power function (since its similar to online code)
+	⁃	rearrange some of the function order in val_derv() (since its similar to online code)
+	⁃	 add comments to ALL CODE
+	⁃	add docstrings for the extensions 
+
+Testing 
+
+	⁃	set up GitHub Actions to accommodate CI requirements
+	⁃	add testing code for ALL VECTOR CASES
+	⁃	add a couple test cases for embedded functions
+	⁃	add testing script for all optimizers 
+
+Documentation 
+
+	⁃	update background section 
+	⁃	(add a small section for each optimizer)
+	⁃	update “How to use” package section
+	⁃	 avoid using numpy and instead show the user how they can build input functions using OUR val_derv class
+	⁃	update “how to use” package section to include optimizer use cases
+	⁃	update the how to install section to include where/how to install optimizers module
+	⁃	update “introduction”
+	⁃	include small section on optimizer extra features (why optimizers are important)
+	⁃	update software organization
+	⁃	include optimizer code + where it lives  + testing files for it
+	⁃	update directory structure  + image
+	⁃	answer question of “Should developers and consumers follow a different installation procedure?”
+	⁃	update implementation details:
+	⁃	extension needs to be described, additional information/background on extension, and any math/background for extension 
+	⁃	see milestone guidelines to see what else to include here
+	⁃	ADD a future section
+	⁃	see milestone guidelines for what to include
+	⁃	Add a broader impact and inclusively statement
+	⁃	half a page in length
+	⁃	include this in the readme and also in the documentation
+
+
+VIDEO
+
+	⁃	create a 15 minute video 
