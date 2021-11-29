97217ebe0d29610594cdeda80eed5332fdcf192e
aditimemani-harvard <aditimemani@g.harvard.edu>
2021-11-29 03:47:02 -0500

    docstrings for optimizers

--- ad_AHJZ/forward_mode.py
+++ ad_AHJZ/forward_mode.py
@@ -7,7 +7,7 @@
 #################################################################################
 
 import numpy as np
-from ad_AHJZ.val_derv import val_derv
+from val_derv import val_derv
 
 
 def combine_vector_inputs(function_list, n_col):
--- ad_AHJZ/optimizers.py
+++ ad_AHJZ/optimizers.py
@@ -27,7 +27,44 @@ x3 = 2
 f_x3 = lambda x: (x-1)**2 +5 
 x_list = [x1, x2, x3]
 fx_list = [f_x1, f_x2, f_x3]
+
+
+
 def adam_optimizer(x,f_x, num_iter, alpha = 0.01, beta1=.9, beta2=.999, epsilon = 1e-8 ):
+    """
+    Parameters
+    ----------
+    x: the variable input (can be in either scalar or vector form)
+    f_x: the function you would like to obtain the minimum for
+    num_iter: the numnber of interations
+    alpha = 0.01: default learning rate or the gradiant descent
+    beta1=.9: default value for the exponential decay of the first moment
+    beta2=.999: default value for the exponential decay of the second moment
+    epsilon = 1e-8: default value to assure that ZeroDivisionError is not raised
+
+    Returns
+    -------
+    opt_time: The time it takes to run the optimizer in seconds
+    val: the position of the minimum value
+    curr_val: the minimum value (can be in either scalar or vector form)
+    
+    Examples
+    --------
+    >>> x = 1
+    >>> f_x = lambda x: (-1*np.log(x))+(np.exp(x)*x**4)/10
+    >>> adam_optimizer(x, f_x, 1000)
+    (0.19765210151672363, 0.26172998379097046, array([0.94233316]))
+    
+    >>> x = np.array([1,-1])
+    >>> f_x = lambda x,y:x**3 +y**2
+    >>> adam_optimizer(x, f_x, 1000)
+    (0.09849190711975098, 6.03886825409073e-06, array([1.82103595e-02, 1.81385270e-21]))
+    
+    >>> x = 2
+    >>> f_x = lambda x: (x-1)**2 +5
+    >>> adam_optimizer(x, f_x, 1000)
+    (0.08600378036499023, 5.0, array([1.]))
+    """
     start = time.time()
     if beta1 >=0 and beta1<1:
         if beta2 >=0 and beta2<1:
@@ -62,6 +99,40 @@ def adam_optimizer(x,f_x, num_iter, alpha = 0.01, beta1=.9, beta2=.999, epsilon 
     
 
 def NADAM (x,f_x, num_iter, alpha = 0.01, beta1=.9, beta2=.999, epsilon = 1e-8 ):
+    """
+    Parameters
+    ----------
+    x: the variable input (can be in either scalar or vector form)
+    f_x: the function you would like to obtain the minimum for
+    num_iter: the numnber of interations
+    alpha = 0.01: default learning rate or the gradiant descent
+    beta1=.9: default value for the exponential decay of the first moment
+    beta2=.999: default value for the exponential decay of the second moment
+    epsilon = 1e-8: default value to assure that ZeroDivisionError is not raised
+
+    Returns
+    -------
+    opt_time: The time it takes to run the optimizer in seconds
+    val: the position of the minimum value
+    curr_val: the minimum value (can be in either scalar or vector form)
+    
+    Examples
+    --------
+    >>> x = 1
+    >>> f_x = lambda x: (-1*np.log(x))+(np.exp(x)*x**4)/10
+    >>> NADAM(x, f_x, 1000)
+    (0.17513608932495117, 0.26172998379097046, array([0.94233316]))
+    
+    >>> x = np.array([1,-1])
+    >>> f_x = lambda x,y:x**3 +y**2
+    >>> NADAM(x, f_x, 1000)
+    (0.09782099723815918, 6.246798742956698e-06, array([ 1.8417012e-02, -2.7649170e-21]))
+    
+    >>> x = 2
+    >>> f_x = lambda x: (x-1)**2 +5
+    >>> NADAM(x, f_x, 1000)
+    (0.07419586181640625, 5.0, array([1.]))
+    """
     start = time.time()
     if beta1 >=0 and beta1<1:
         if beta2 >=0 and beta2<1:
@@ -94,6 +165,39 @@ def NADAM (x,f_x, num_iter, alpha = 0.01, beta1=.9, beta2=.999, epsilon = 1e-8 )
 #print()
 #nag uses only 1 past gradient moment 
 def NAG(x,f_x, num_iter, alpha=.1, gamma=0.9, epsilon = 1e-8 ):
+    """
+    Parameters
+    ----------
+    x: the variable input (can be in either scalar or vector form)
+    f_x: the function you would like to obtain the minimum for
+    num_iter: the numnber of interations
+    alpha = 0.1: default learning rate or the gradiant descent
+    gamma=.9: default value for the exponential decay
+    epsilon = 1e-8: default value to assure that ZeroDivisionError is not raised
+
+    Returns
+    -------
+    opt_time: The time it takes to run the optimizer in seconds
+    val: the position of the minimum value
+    curr_val: the minimum value (can be in either scalar or vector form)
+    
+    Examples
+    --------
+    >>> x = 1
+    >>> f_x = lambda x: (-1*np.log(x))+(np.exp(x)*x**4)/10
+    >>> NAG(x, f_x, 1000)
+    (0.206679105758667, 0.26177237171374856, array([0.94615669]))
+    
+    >>> x = np.array([1,-1])
+    >>> f_x = lambda x,y:x**3 +y**2
+    >>> NAG(x, f_x, 1000)
+    (0.09226107597351074, 0.14092469295850776, array([ 0.38994091, -0.28571429]))
+    
+    >>> x = 2
+    >>> f_x = lambda x: (x-1)**2 +5
+    >>> NAG(x, f_x, 1000)
+    (0.07019376754760742, 5.081632653061225, array([1.28571429]))
+    """
     start = time.time()
     if gamma >=0 and gamma<1:
         vt = 0
@@ -129,6 +233,39 @@ def NAG(x,f_x, num_iter, alpha=.1, gamma=0.9, epsilon = 1e-8 ):
 #        print()
 
 def RMSprop(x,f_x, num_iter, alpha = 0.01, beta=.9, epsilon = 1e-8 ):
+    """
+    Parameters
+    ----------
+    x: the variable input (can be in either scalar or vector form)
+    f_x: the function you would like to obtain the minimum for
+    num_iter: the numnber of interations
+    alpha = 0.01: default learning rate or the gradiant descent
+    beta =.9: default value for the exponential decay
+    epsilon = 1e-8: default value to assure that ZeroDivisionError is not raised
+
+    Returns
+    -------
+    opt_time: The time it takes to run the optimizer in seconds
+    val: the position of the minimum value
+    curr_val: the minimum value (can be in either scalar or vector form)
+    
+    Examples
+    --------
+    >>> x = 1
+    >>> f_x = lambda x: (-1*np.log(x))+(np.exp(x)*x**4)/10
+    >>> RMSprop(x, f_x, 1000)
+    (0.3034090995788574, 0.2618028370373199, array([0.93730206]))
+    
+    >>> x = np.array([1,-1])
+    >>> f_x = lambda x,y:x**3 +y**2
+    >>> RMSprop(x, f_x, 1000)
+    (0.08888602256774902, 2.4997500000081948e-05, array([ 4.34424616e-06, -4.99974999e-03]))
+    
+    >>> x = 2
+    >>> f_x = lambda x: (x-1)**2 +5
+    >>> RMSprop(x, f_x, 1000)
+    (0.07326507568359375, 5.0, array([1.]))
+    """
     start = time.time()
     if beta >=0 and beta<1:
         vt = 0 
@@ -150,6 +287,39 @@ def RMSprop(x,f_x, num_iter, alpha = 0.01, beta=.9, epsilon = 1e-8 ):
     return opt_time, val, curr_val
 
 def momentum(x,f_x, num_iter, alpha = 0.01, beta=.9, epsilon = 1e-8):
+    """
+    Parameters
+    ----------
+    x: the variable input (can be in either scalar or vector form)
+    f_x: the function you would like to obtain the minimum for
+    num_iter: the numnber of interations
+    alpha = 0.01: default learning rate or the gradiant descent
+    beta=.9: default value for the exponential decay
+    epsilon = 1e-8: default value to assure that ZeroDivisionError is not raised
+
+    Returns
+    -------
+    opt_time: The time it takes to run the optimizer in seconds
+    val: the position of the minimum value
+    curr_val: the minimum value (can be in either scalar or vector form)
+    
+    Examples
+    --------
+    >>> x = 1
+    >>> f_x = lambda x: (-1*np.log(x))+(np.exp(x)*x**4)/10
+    >>> momentum(x, f_x, 1000)
+    (0.24898195266723633, 0.26172998379097046, array([0.94233316]))
+    
+    >>> x = np.array([1,-1])
+    >>> f_x = lambda x,y:x**3 +y**2
+    >>> momentum(x, f_x, 1000)
+    (0.08585095405578613, 2.7605629377339922e-05, array([ 3.02226506e-02, -3.30135704e-12]))
+    
+    >>> x = 2
+    >>> f_x = lambda x: (x-1)**2 +5
+    >>> momentum(x, f_x, 1000)
+    (0.06420779228210449, 5.0, array([1.]))
+    """
     start = time.time()
     if beta >=0 and beta<1:
             mt = 0 
@@ -170,6 +340,37 @@ def momentum(x,f_x, num_iter, alpha = 0.01, beta=.9, epsilon = 1e-8):
     opt_time = end - start
     return opt_time, val, curr_val 
 def BFGS(x,f_x, num_iter, alpha = 0.01):
+    """
+    Parameters
+    ----------
+    x: the variable input (can be in either scalar or vector form)
+    f_x: the function you would like to obtain the minimum for
+    num_iter: the numnber of interations
+    alpha = 0.01: default learning rate or the gradiant descent
+    
+    Returns
+    -------
+    opt_time: The time it takes to run the optimizer in seconds
+    val: the position of the minimum value
+    curr_val: the minimum value (can be in either scalar or vector form)
+    
+    Examples
+    --------
+    >>> x = 1
+    >>> f_x = lambda x: (-1*np.log(x))+(np.exp(x)*x**4)/10
+    >>> BFGS(x, f_x, 1000)
+    (0.2875189781188965, 0.2617299838095016, array([0.94233569]))
+    
+    >>> x = np.array([1,-1])
+    >>> f_x = lambda x,y:x**3 +y**2
+    >>> BFGS(x, f_x, 1000)
+    (0.12035322189331055, 2.864368146437936e-07, array([ 6.57872856e-03, -4.13716985e-05]))
+    
+    >>> x = 2
+    >>> f_x = lambda x: (x-1)**2 +5
+    >>> BFGS(x, f_x, 1000)
+    ((0.08925986289978027, 5.000000001826295, array([1.00004274]))
+    """
     start = time.time()
     if np.isscalar(x):
         n= 1
@@ -198,6 +399,37 @@ def BFGS(x,f_x, num_iter, alpha = 0.01):
     return opt_time, val, curr_val  
 
 def broyden(x,f_x, num_iter, alpha = 0.01):
+    """
+    Parameters
+    ----------
+    x: the variable input (can be in either scalar or vector form)
+    f_x: the function you would like to obtain the minimum for
+    num_iter: the numnber of interations
+    alpha = 0.01: default learning rate or the gradiant descent
+    
+    Returns
+    -------
+    opt_time: The time it takes to run the optimizer in seconds
+    val: the position of the minimum value
+    curr_val: the minimum value (can be in either scalar or vector form)
+    
+    Examples
+    --------
+    >>> x = 1
+    >>> f_x = lambda x: (-1*np.log(x))+(np.exp(x)*x**4)/10
+    >>> broyden(x, f_x, 1000)
+    (0.2750861644744873, 0.2617299838095016, array([0.94233569]))
+    
+    >>> x = np.array([1,-1])
+    >>> f_x = lambda x,y:x**3 +y**2
+    >>> broyden(x, f_x, 1000)
+    (0.1135101318359375, 3.060640040173604e-07, array([ 6.72566138e-03, -4.28010827e-05]))
+    
+    >>> x = 2
+    >>> f_x = lambda x: (x-1)**2 +5
+    >>> broyden(x, f_x, 1000)
+    broyden: (0.0851430892944336, 5.000000001826295, array([1.00004274]))
+    """
     start = time.time()
     if np.isscalar(x):
         n= 1
@@ -224,25 +456,60 @@ def broyden(x,f_x, num_iter, alpha = 0.01):
         
 
 for x, f_x in zip (x_list, fx_list):
+    print('x=',x)
+    print('f_x=',f_x)
     print('ADAM:',adam_optimizer(x, f_x, 1000)) 
     print()
+    print('x=',x)
+    print('f_x=',f_x)
     print('NADAM:',NADAM(x, f_x, 1000)) 
     print() 
+    print('x=',x)
+    print('f_x=',f_x)
     print('NAG:',NAG(x, f_x, 1000)) 
     print()
+    print('x=',x)
+    print('f_x=',f_x)
     print('RMSprop:', RMSprop(x, f_x, 1000)) 
     print()
+    print('x=',x)
+    print('f_x=',f_x)
     print('momentum:', momentum(x, f_x, 1000)) 
     print()
+    print('x=',x)
+    print('f_x=',f_x)
     print('BFGS:', BFGS(x, f_x, 1000)) 
     print()
+    print('x=',x)
+    print('f_x=',f_x)
     print('broyden:', broyden(x, f_x, 1000)) 
     print()
 
 x = 3
 f_x = lambda x: (x - 1)**2
 
-def newtonScalar(x,f_x,num_iter, epsilon):
+def newtonScalar(x,f_x,num_iter, epsilon=1e-6):
+    """
+    Parameters
+    ----------
+    x: the variable input (can ONLY be scalar)
+    f_x: the function you would like to use
+    num_iter: the numnber of interations
+    epsilon = 1e-6: default value to assure that ZeroDivisionError is not raised
+
+    Returns
+    -------
+    opt_time: The time it takes to run the optimizer in seconds
+    val: the position of the minimum value
+    curr_val: the minimum value (can be in either scalar or vector form)
+    
+    Examples
+    --------
+    >>> x = 3
+    >>> f_x = lambda x: (x - 1)**2
+    >>> newtonScalar(x, f_x, 100, 1e-6)
+    (0.0013539791107177734, array([1.00000191]), 3.637978807091713e-12)
+    """
     start = time.time()
     for n in range(1,num_iter+1):
         fm = forward_mode(x,f_x)
